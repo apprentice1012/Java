bfd08d23a0f04be72fe2d76cf0a660624e0ef06c
apprentice1012 <126549223+apprentice1012@users.noreply.github.com>
2023-07-21 18:11:24 +0800

    Update IndexOfDemo.java

--- Demo/StringDemo/IndexOfDemo.java
+++ Demo/StringDemo/IndexOfDemo.java
@@ -15,7 +15,9 @@ public class IndexOfDemo {
     public static void main(String[] args) {
         String str = "abcd";
         System.out.println(str.indexOf("a"));//判断字符a是否在str中
+        System.out.println(str.indexOf(99));//判断字符c是否在str中，99为c的ASCII码
         System.out.println(str.indexOf("e"));//e不再str中所以返回-1
         System.out.println(str.indexOf("d",1));//判断d是否在str的起始下标为1之后的字符串(bcd)中
+        System.out.println(str.indexOf("",4));//验证源码中if (fromIndex >= sourceCount) {return (targetCount == 0 ? sourceCount : -1);}
     }
 }
